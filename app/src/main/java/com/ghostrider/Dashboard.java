c01038e413ba4a6b9555e17d884cc452ea81cbbe
Atif Niyaz <atifniyaz11@gmail.com>
2017-02-25 10:33:27 -0600

    Dynamic color change between fragments

--- app/src/main/java/com/ghostrider/Dashboard.java
+++ app/src/main/java/com/ghostrider/Dashboard.java
@@ -1,9 +1,11 @@
 package com.ghostrider;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import com.ghostrider.fragment.Account;
 import com.ghostrider.fragment.Dash;
@@ -15,6 +17,7 @@ import com.roughike.bottombar.OnTabSelectListener;
 
 public class Dashboard extends AppCompatActivity {
 
+    public static final String[] colors = {"#9C27B0", "#e91e63", "#2196f3"};
     public Fragment currentFragment;
 
     @Override
@@ -35,12 +38,15 @@ public class Dashboard extends AppCompatActivity {
             public void onTabSelected(int tabId) {
                 switch (tabId) {
                     case R.id.tab_home:
+                        changeColors(R.drawable.background_dash, colors[0]);
                         changeFragment(Dash.getInstance());
                         break;
                     case R.id.tab_run:
+                        changeColors(R.drawable.background_run, colors[1]);
                         changeFragment(Run.getInstance());
                         break;
                     case R.id.tab_race:
+                        changeColors(R.drawable.background_race, colors[2]);
                         changeFragment(Race.getInstance());
                         break;
                     case R.id.tab_stats:
@@ -64,5 +70,13 @@ public class Dashboard extends AppCompatActivity {
         transaction.replace(R.id.dashContainer, currentFragment = fragment);
         transaction.addToBackStack(null);
         transaction.commit();
+    }
+
+    void changeColors(int rescID, String color) {
+        View _backgroundView = findViewById(R.id.dashContainer).getRootView();
+        _backgroundView.setBackgroundResource(rescID);
+
+        BottomBar _bar = (BottomBar) findViewById(R.id.dashBar);
+        _bar.setActiveTabColor(Color.parseColor(color));
     }
 }
